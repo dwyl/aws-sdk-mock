4a9b802e66a613145f9ec43d9948b515ed7e6434
Joost Farla <joost@apiwise.nl>
2019-02-15 13:09:49 +0100

    Fix wrong TypeScript definitions

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 declare module 'aws-sdk-mock' {
-  function mock(service: string, method: string, replace: string): void;
+  function mock(service: string, method: string, replace: any): void;
 
   function mock(
     service: string,
@@ -7,15 +7,14 @@ declare module 'aws-sdk-mock' {
     replace: (params: any, callback: (err: any, data: any) => void) => void
   ): void;
 
-  function remock(service: string, method: string, replace: string): void;
+  function remock(service: string, method: string, replace: any): void;
   function remock(
     service: string,
     method: string,
     replace: (params: any, callback: (err: any, data: any) => void) => void
   ): void;
 
-  function restore(service: string): void;
-  function restore(service: string, method: string): void;
+  function restore(service?: string, method?: string): void;
 
   function setSDK(path: string): void;
   function setSDKInstance(instance: object): void;
